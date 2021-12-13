e1c2fb1862b6179eb8d8fd864e2298737b36304c
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-12-13 15:46:07 +0800

    added comments and timestamp to doctor and patient models

--- models/patient.js
+++ models/patient.js
@@ -1,86 +1,96 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PatientSchema = new Schema({
-  nric: {           //required to id the patient
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  DOB: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  allergies: [
-    {
+const PatientSchema = new Schema(
+  {
+    nric: {
+      //required to id the patient
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      //hashed password string
+      type: String,
+      required: true,
+    },
+    DOB: {
+      //use format DD/MM/yyyy. type string as easy to parse to date format, saving as date will include timestamp
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    allergies: [
+      {
+        type: String,
+      },
+    ],
+    insuranceID: {
       type: String,
     },
-  ],
-  insuranceID: {
-    type: String,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-  },
-  dependents: [     //mainly for Paediatrics
-    {
-      nric: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      relation: {
+    gender: {
+      type: String,
+      required: true,
+    },
+    img: {
+      //temporary use a placeholder image. Look into storing actual images in db as a stretch goal
+      type: String,
+    },
+    dependents: [
+      //mainly for Paediatrics
+      {
+        nric: {
           type: String,
-          required: true
-      },
-      DOB: {
-        type: String,
-        required: true,
-      },
-      address: {
-        type: String,
-        required: true,
-      },
-      allergies: [
-        {
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        relation: {
+          type: String,
+          required: true,
+        },
+        DOB: {
+          type: String,
+          required: true,
+        },
+        address: {
+          type: String,
+          required: true,
+        },
+        allergies: [
+          {
+            type: String,
+          },
+        ],
+        insuranceID: {
           type: String,
         },
-      ],
-      insuranceID: {
-        type: String,
+        gender: {
+          type: String,
+          required: true,
+        },
+        img: {
+          type: String,
+        },
       },
-      gender: {
-        type: String,
-        required: true,
-      },
-      img: {
-        type: String,
-      },
-    },
-  ],
-});
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Patient", PatientSchema);
