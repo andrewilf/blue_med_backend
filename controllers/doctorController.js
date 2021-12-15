fb939da36ef04073b1cedcb1804ddf6d8faf66a7
andrewfaulkner <andrewfau1kn3r@gmail.com>
2021-12-15 17:10:18 +0800

    added get and post routes for doctor model

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -3,17 +3,66 @@ require("dotenv").config();
 const Doctor = require("../models/doctor");
 const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   const PatientAll = await Patient.find({});
-//   //console.log(allScientists)
-//   res.send(PatientAll);
-// });
+router.get("/", async (req, res) => {
+  console.log("get all doctors");
+  const doctorAll = await Doctor.find({});
+  res.send(doctorAll);
+});
 
-// router.post("/", async (req, res) => {
-//     const PatientAll = await Patient.create({
+router.get("/:doctorID", async (req, res) => {
+  try {
+    const doctorID = req.params.doctorID;
+    console.log("search for doctor by _id");
+    const doctorGetOne = await Doctor.findOne({ _id: doctorID });
+    if (doctorGetOne !== null) {
+      res.send(doctorGetOne);
+    } else {
+      //_id was of the correct format but no doctor was found
+      res.status(404).send("doctor not found");
+    }
+  } catch (error) {
+    //likely the doctorID was not a string of 12 bytes or a string of 24 hex characters
+    console.error(error);
+    res.status(400).send("error when finding doctor, bad input");
+  }
+});
 
-//     });
-//     //console.log(allScientists)
-//     res.send(PatientAll);
-//   });
+router.get("/:searchField/:searchValue", async (req, res) => {
+  try {
+    const searchField = req.params.searchField;
+    const searchValue = req.params.searchValue;
+    console.log(`search by field: ${searchField}`);
+    const checkFieldExists = Object.keys(Doctor.schema.tree).find(
+      //checks if the searchField is a valid field for the model
+      (element) => element === searchField
+    );
+    const doctorsGet = await Doctor.find({ [searchField]: searchValue });
+    if (doctorsGet.length !== 0 && checkFieldExists) {
+      //return valid response
+      res.send(doctorsGet);
+    } else if (!checkFieldExists) {
+      //searchField is not valid
+      res.status(400).send(`"${searchField}" is not a valid field`);
+    } else {
+      //doctor field exists but no doctor was found
+      res
+        .status(404)
+        .send(`no doctors were found for the field: ${searchField}`);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("error when finding doctor");
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const doctorCreate = await Doctor.create(req.body);
+    res.send(doctorCreate);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("error when adding doctor, bad input");
+  }
+});
+
 module.exports = router;
