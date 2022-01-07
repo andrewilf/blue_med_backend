1542fab3d81382216e1a39a9a2cc25d6d025b9b6
Rosherna Chin <rosherna@Roshernas-MacBook-Air.local>
2022-01-07 17:05:49 +0800

    removed commented code in pastAppControllers

--- controllers/pastAppController.js
+++ controllers/pastAppController.js
@@ -3,20 +3,6 @@ require("dotenv").config();
 const pastAppointment = require("../models/past_appointment");
 const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   const PatientAll = await Patient.find({});
-//   //console.log(allScientists)
-//   res.send(PatientAll);
-// });
-
-// router.post("/", async (req, res) => {
-//     const PatientAll = await Patient.create({
-
-//     });
-//     //console.log(allScientists)
-//     res.send(PatientAll);
-//   });
-
 //POST 
 //create a pastAppointment
 router.post('/', async (req, res) => {
